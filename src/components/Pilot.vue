0c3b79a12f36a05974485215a1c49feb5ed1d881
Robert Babaev <github@robertbabaev.tech>
2024-05-11 17:47:59 -0400

    finally get the briefing up and running

--- src/components/Pilot.vue
+++ src/components/Pilot.vue
@@ -1,32 +1,26 @@
 <template>
-	<div class="pilot-wrapper">
+  <div class="pilot-wrapper">
     <div class="pilot-column" @click="pilotModal">
       <img :src="pilotInfo.cloud_portrait" class="portrait" />
       <div class="pilot-info">
         <div class="callsign">
           <h1>TAC Name</h1>
-          <h2>{{ pilotInfo.bloodhound_callsign }}</h2>
+          <h2>{{ pilotInfo.callsign }}</h2>
         </div>
         <div class="name">
           <h1>Callsign</h1>
-          <h2>{{ pilotInfo.callsign }}</h2>
+          <h2>{{ pilotInfo.bloodhound_callsign }}</h2>
         </div>
         <div class="name">
           <h1>Name or Legal Alias</h1>
           <h2>{{ pilotInfo.name }}</h2>
         </div>
-        <div
-          v-if="pilotInfo.age || pilotInfo.pob"
-          class="age-pob">
-          <div
-            v-if="pilotInfo.pob.length > 0"
-            class="pob">
+        <div v-if="pilotInfo.age || pilotInfo.pob" class="age-pob">
+          <div v-if="pilotInfo.pob.length > 0" class="pob">
             <h1>Place of Birth</h1>
             <h2>{{ pilot.pob }}</h2>
           </div>
-          <div
-            v-if="pilotInfo.age.length > 0"
-            class="age">
+          <div v-if="pilotInfo.age.length > 0" class="age">
             <h1>Subjective Age</h1>
             <h2>{{ pilot.age }}</h2>
           </div>
@@ -36,9 +30,7 @@
         <p id="output" ref="output" />
       </div>
     </div>
-    <div
-      v-if="!pilotInfo.bondId"
-      class="gear-column">
+    <div v-if="!pilotInfo.bondId" class="gear-column">
       <div class="gear-row">
         <div class="armor">
           <h1>Pilot Armor</h1>
@@ -70,9 +62,7 @@
         </div>
       </div>
     </div>
-    <div
-      v-if="pilotInfo.bondId"
-      class="bonds-column">
+    <div v-if="pilotInfo.bondId" class="bonds-column">
       <div class="bonds">
         <div class="bond">
           <h1>Bond</h1>
@@ -80,62 +70,48 @@
         </div>
         <div class="experience">
           <h1>Experience</h1>
-          <ProgressBar
-            :value="pilotInfo.xp"
-            :max="8"
-            color="rgba(125, 187, 187, 1)"
-          />
+          <ProgressBar :value="pilotInfo.xp" :max="8" color="rgba(125, 187, 187, 1)" />
         </div>
         <div class="stress">
           <h1>Stress</h1>
-          <ProgressBar
-            :value="pilotInfo.stress"
-            :max="8" color="#F00" />
+          <ProgressBar :value="pilotInfo.stress" :max="8" color="#F00" />
         </div>
       </div>
-      <div
-        v-if="pilotInfo.burdens.length > 0"
-        class="burdens">
-        <Burden
-          v-for="item in pilotInfo.burdens"
-          :key="item.id"
-          :burden="item"
-          :animate="animate"/>
-    </div>
-    <div
-      v-else
-      class="gear-column">
-      <div class="gear-row">
-        <div class="armor">
-          <h1>Pilot Armor</h1>
-          <h2>{{ pilotInfo.loadout.armor[0].flavorName }}</h2>
+      <div v-if="pilotInfo.burdens.length > 0" class="burdens">
+        <Burden v-for="item in pilotInfo.burdens" :key="item.id" :burden="item" :animate="animate" />
+      </div>
+      <div v-else class="gear-column">
+        <div class="gear-row">
+          <div class="armor">
+            <h1>Pilot Armor</h1>
+            <h2>{{ pilotInfo.loadout.armor[0].flavorName }}</h2>
+          </div>
+          <div class="gear">
+            <h1>Pilot Gear</h1>
+            <h2>{{ pilotInfo.loadout.gear[0].flavorName }}</h2>
+          </div>
         </div>
-        <div class="gear">
-          <h1>Pilot Gear</h1>
-          <h2>{{ pilotInfo.loadout.gear[0].flavorName }}</h2>
+        <div class="gear-row">
+          <div class="weapon">
+            <h1>Pilot Weapon</h1>
+            <h2>{{ pilotInfo.loadout.weapons[0].flavorName }}</h2>
+          </div>
+          <div class="gear">
+            <h1>Pilot Gear</h1>
+            <h2>{{ pilotInfo.loadout.gear[1].flavorName }}</h2>
+          </div>
+        </div>
+        <div class="gear-row">
+          <div class="weapon">
+            <h1>Pilot Weapon</h1>
+            <h2>{{ pilotInfo.loadout.weapons[1].flavorName }}</h2>
+          </div>
+          <div class="gear">
+            <h1>Pilot Gear</h1>
+            <h2>{{ pilotInfo.loadout.gear[2].flavorName }}</h2>
+          </div>
         </div>
       </div>
-      <div class="gear-row">
-        <div class="weapon">
-          <h1>Pilot Weapon</h1>
-          <h2>{{ pilotInfo.loadout.weapons[0].flavorName }}</h2>
-        </div>
-        <div class="gear">
-          <h1>Pilot Gear</h1>
-          <h2>{{ pilotInfo.loadout.gear[1].flavorName }}</h2>
-        </div>
-      </div>
-      <div class="gear-row">
-        <div class="weapon">
-          <h1>Pilot Weapon</h1>
-          <h2>{{ pilotInfo.loadout.weapons[1].flavorName }}</h2>
-        </div>
-        <div class="gear">
-          <h1>Pilot Gear</h1>
-          <h2>{{ pilotInfo.loadout.gear[2].flavorName }}</h2>
-        </div>
-      </div>
-    </div>
     </div>
     <div class="mech-column" @click="mechModal">
       <div class="mech-info">
@@ -144,7 +120,8 @@
           <h2>{{ activeMech.name }}</h2>
         </div>
         <div class="manufacturer">
-          <svg :data-src="`${mechManufacturerIcon}#Content`" style="width:48px; height:48px; fill:var(--primary-color)"></svg>
+          <svg :data-src="`${mechManufacturerIcon}#Content`"
+            style="width:48px; height:48px; fill:var(--primary-color)"></svg>
           <div class="manufacturer-info">
             <h1>{{ activeMech.manufacturer }}</h1>
             <h2>{{ activeMech.frame_name }}</h2>
@@ -174,6 +151,30 @@ import MechModal from '@/components/modals/MechModal.vue'
 import ProgressBar from '@/components/ProgressBar.vue'
 import Burden from '@/components/Burden.vue'
 
+async function processImports() {
+  const manufacturers = import.meta.glob('@/assets/manufacturers/*.json')
+  const frames = import.meta.glob('@/assets/frames/*.json')
+  const weapons = import.meta.glob('@/assets/weapons/*.json')
+  const systems = import.meta.glob('@/assets/systems/*.json')
+  const dataCollection = [
+    { name: "manufacturers", files: manufacturers },
+    { name: "frames", files: frames },
+    { name: "weapons", files: weapons },
+    { name: "systems", files: systems },
+  ];
+  const addedData = {};
+
+  for (let importItem of dataCollection) {
+    let filePromises = Object.keys(importItem.files).map(path => importItem.files[path]());
+    const results = await Promise.all(filePromises)
+    results.forEach(content => {
+      addedData[importItem.name] ||= []
+      addedData[importItem.name] = [...addedData[importItem.name], ...content.default];
+    });
+  }
+  return addedData;
+}
+
 export default {
   components: {
     Burden,
@@ -190,10 +191,25 @@ export default {
     },
   },
   data() {
+    // const returnValue = {
+    //   knownGear: [...lancerData.pilot_gear, ...nrfawData.pilot_gear],
+    //   activeMech: {},
+    //   bond: {},
+    //   addedDataPromise: processImports()
+    // }
+
+
+    // .then(addedData => {
+    //   returnValue.addedData = addedData;
+    // })
+    // .catch(err => {
+    //   console.log(err);
+    // })
     return {
       knownGear: [...lancerData.pilot_gear, ...nrfawData.pilot_gear],
       activeMech: {},
       bond: {},
+      addedDataPromise: processImports()
     }
   },
   computed: {
@@ -207,6 +223,7 @@ export default {
       return [...lancerData.weapons, ...ktbData.weapons, ...nrfawData.weapons, ...longrimData.weapons]
     },
     mechSystems() {
+      console.log(this.addedData.systems);
       return [...lancerData.systems, ...ktbData.systems, ...nrfawData.systems, ...longrimData.systems]
     },
     mechManufacturerIcon() {
@@ -223,13 +240,13 @@ export default {
         identName += `${part}.`
       })
       identName += identFirstName;
-			return `Union Administrative RM-4 Pilot Identification Protocol (IDENT) Record ${identName}: ${this.pilot.id} // ${this.pilot.background} // LOADOUT ${this.pilot.loadout.id} - MECH ${this.pilot.mechs[0].id} // HARDPOINTS ${this.pilot.mechs[0].loadouts[0].id}`;
-		},
+      return `Union Administrative RM-4 Pilot Identification Protocol (IDENT) Record ${identName}: ${this.pilot.id} // ${this.pilot.background} // LOADOUT ${this.pilot.loadout.id} - MECH ${this.pilot.mechs[0].id} // HARDPOINTS ${this.pilot.mechs[0].loadouts[0].id}`;
+    },
     pilotInfo() {
       const info = this.pilot
 
       var resolveGear = (type, item, idx, arr) => {
-        item = item || {id: "", flavorName: ""};
+        item = item || { id: "", flavorName: "" };
         const gear = this.knownGear.find((obj) => { return item.id === obj.id }) || null;
         item.flavorName = gear?.name || "ERR: DATA NOT FOUND";
         arr[idx] = item;
@@ -237,7 +254,7 @@ export default {
 
       info.loadout.armor.forEach((item, index, array) => resolveGear('armor', item, index, array));
       info.loadout.weapons.forEach((item, index, array) => resolveGear('weapon', item, index, array));
-      info.loadout.gear.forEach((item, index, array) =>resolveGear('gear', item, index, array));
+      info.loadout.gear.forEach((item, index, array) => resolveGear('gear', item, index, array));
 
       return info;
     },
@@ -278,6 +295,20 @@ export default {
       this.activeMech.frame_name = frame.name
       this.activeMech.manufacturer = frame.source
       this.activeMech.mechtype = frame.mechtype.join(' // ')
+
+      this.addedDataPromise.then(addedData => {
+        knownFrames.push(...addedData.frames)
+        let frame = knownFrames.find((obj) => {
+          return obj.id === this.activeMech.frame
+        })
+
+        if (!frame)
+          frame = lancerData.frames[0]
+
+        this.activeMech.frame_name = frame.name
+        this.activeMech.manufacturer = frame.source
+        this.activeMech.mechtype = frame.mechtype.join(' // ')
+      })
     },
     animatePilotCode() {
       this.typer = new TypeIt(this.$refs.output, {
@@ -286,8 +317,8 @@ export default {
         lifeLike: false,
         cursor: true,
         startDelete: false,
-        loop:true,
-        loopDelay:10000,
+        loop: true,
+        loopDelay: 10000,
         strings: this.pilotCode,
         beforeString: () => {
           this.$refs.output?.scrollIntoView({ block: 'end' })
